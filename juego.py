5c09cee374ebdc820a1c96dacb4afc375845133f
Juan Diego <ajuandiego036@gmail.com>
2023-03-30 16:34:51 -0500

    Tercer commit. Cambios en notas

--- juego.py
+++ juego.py
@@ -15,7 +15,6 @@ import pygame
 
 #Falta buscar mas sobre como crearemos la interfaz
 
-
 diametro_anchura = 1000
 diametro_altura = 500
 color_negro = (0, 0, 0)
@@ -44,7 +43,7 @@ class Nave(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.imagen = pygame.imagen.load("aqui ira la imagen de la nave").convert()
-        self.rect= self.rect.imagen.get_rect() #aqui se optiene la recta o el cuadro del sprite, POST; rect es rectificar
+        self.rect= self.rect.imagen.get_rect() #aqui se optiene la recta o el cuadro del sprite
         self.rect.centrox = diametro_anchura // 2 #aqui se pone en pantalla por asi decirlo, falta investigar mas sobre esto
         self.rect.abajo = diametro_altura - 10
         self.movimiento_en_x = 0
