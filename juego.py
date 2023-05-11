76691f84142f18ba1aafe85abc939d941372299e
Juan Diego <ajuandiego036@gmail.com>
2023-05-11 14:52:58 -0500

    Primer parte

--- juego.py
+++ juego.py
@@ -6,8 +6,6 @@
 #---------------------------------------------------------------------
 #Importaciones:
 
-#
-
 import pygame, random
 
 #-----------------------------------------------------------------------
@@ -25,7 +23,7 @@ color_blanco = (255, 255, 255)
 
 pygame.init()
 pygame.mixer.init() #Esto se utiliza para poner musica en el juego
-interfaz = pygame.display.set_mode(WIDTH, HEIGTH)
+interfaz = pygame.display.set_mode((WIDTH, HEIGTH))
 pygame.display.set_caption("Call Of Dutty")
 tiempo = pygame.time.Clock()
 
@@ -40,22 +38,38 @@ tiempo = pygame.time.Clock()
 #Falta buscar sobre como mover la nave
 
 class Nave(pygame.sprite.Sprite):
-    def _init_(self):
-        super()._init_()
-        self.imagen = pygame.imagen.load("aqui ira la imagen de la nave").convert()
-        self.rect= self.rect.imagen.get_rect() #aqui se optiene la recta o el cuadro del sprite
-        self.rect.centrox = WIDTH // 2 #aqui se pone en pantalla por asi decirlo, falta investigar mas sobre esto
-        self.rect.abajo = HEIGTH - 10
-        self.movimiento_en_x = 0
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("player.png").convert()
+        self.image.set_colorkey(color_negro)
+        self.rect = self.image.get_rect() #aqui se optiene la recta o el cuadro del sprite
+        self.rect.centerx = WIDTH // 2 #aqui se pone en pantalla por asi decirlo, falta investigar mas sobre esto
+        self.rect.bottom = HEIGTH - 10
+        self.speed_x = 0
+        self.speed_y = 0
 
-    def movimiento(self):
-        self.velocidad_x = 0
+    def update(self):  #Movimiento
+        self.speed_x = 0
+        self.speed_y = 0
         tecla_presionada = pygame.key.get_pressed()
         if tecla_presionada[pygame.K_LEFT]:
-            self.speed_x = -5
+            self.speed_x = -7
         if tecla_presionada[pygame.K_RIGHT]:
-            self.speed_x = 5
+            self.speed_x = 7
+        if tecla_presionada[pygame.K_UP]:
+            self.speed_y = -7
+        if tecla_presionada[pygame.K_DOWN]:
+            self.speed_y = 7
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGTH:
+            self.rect.bottom = HEIGTH
         self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
 
 
     def disparo(self):
@@ -68,8 +82,8 @@ class Nave(pygame.sprite.Sprite):
 #
 
 class Meteoro(pygame.sprite.Sprite):
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
 
     def movimiento(self):
         pass
@@ -80,8 +94,8 @@ class Meteoro(pygame.sprite.Sprite):
 #
 
 class Disparo(pygame.sprite.Sprite):
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
 
 
 # --------------------------------------------------------------------------
@@ -102,11 +116,12 @@ while fps:
         if evento.type == pygame.QUIT:
             fps = False
 
-
     todos_sprites.update()
 
     interfaz.fill(color_negro) #Es un método que se utiliza para rellenar la superficie de la ventana
                                # de visualización del juego con un color específico.
+
+    todos_sprites.draw(interfaz)
 
     pygame.display.flip() #Este metodo se utiliza para actualizar la ventana de
                           # visualización del juego
